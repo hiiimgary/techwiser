7db8d18280c824e487f96aa1dde54f6343a1fe46
gergo.pattantyus <gergo.pattantyus@trendency.hu>
2022-04-10 16:41:37 +0200

    Initial commit

---
+++ app/src/main/java/com/hiimgary/techwiser/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.hiimgary.techwiser
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hiimgary.techwiser.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
